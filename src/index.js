b822168bfcb836634a8d72fe68666454a39e62e5
Anil Anar <anilanar@hotmail.com>
2015-06-07 01:02:31 +0200

    initial working implementation

---
+++ src/index.js
@@ -0,0 +1,49 @@
+(function (window, angular) {
+  window.tentacle = window.tentacle || {};
+  var mockExceptions = [
+    '$http',
+    '$httpBackend',
+    '$rootScope',
+    '$q'
+  ];
+
+  window.tentancle.mock = function (module, name, override, defaultMock) {
+    angular.mock.module(module);
+    override = override || {};
+    defaultVal = defaultVal || {};
+
+    var dependencies = getDependencies(module, name);
+    dependencies = _.difference(dependencies, mockExceptions);
+
+    var mocksObject = createMocksObject(dependencies, override, defaultMock);
+    _.forEach(mocksObject, provideValue);
+    return mocksObject;
+  };
+
+  function getDependencies(module, name) {
+    var invokeQueue = angular.mdoule(moduleName)._invokeQueue;
+    return _.chain(invokeQueue)
+      .unzip().last()
+      .find(function (config) {
+        return _.isEqual(config[0], name);
+      })
+      .last().initial().value();
+  }
+
+  function createMocksObject(dependencies, override, defaultMock) {
+    return _.chain(dependencies)
+      .reduce(function (mockObject, dependency) {
+        mocksObject[dependency] = defaultMock;
+        return mocksObject;
+      }, {})
+      .assign(override)
+      .value();
+  }
+
+  function provideValue(mock, dependency) {
+    angular.mock.module(function ($provide) {
+      $provide.value(dependency, mock);
+    });
+  }
+
+})(window, angular);
