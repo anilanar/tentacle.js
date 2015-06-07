27445685ea506636735761b1dbfc527772ae892c
Anil Anar <anilanar@hotmail.com>
2015-06-07 16:34:21 +0200

    Implemented tentacle.controller and tentacle.async, refactored code

--- src/index.js
+++ src/index.js
@@ -1,46 +1,105 @@
 (function (window, angular) {
-  window.tentacle = {};
 
-  var mockExceptions = [
-    '$http',
-    '$httpBackend',
-    '$rootScope',
-    '$q'
-  ];
+  var dirtyProperties,
+    mockExceptions,
+    globalInjects;
 
-  var globalInjects = [
-    '$rootScope',
-    '$q',
-    '$httpBackend'
-  ];
+  beforeEach(function () {
+    dirtyProperties = [];
+    mockExceptions = [
+      '$http',
+      '$httpBackend',
+      '$rootScope',
+      '$q',
+      '$scope'
+    ];
 
-  window.tentacle.mock = function (module, name, override, defaultMock) {
+    globalInjects = [
+      '$rootScope',
+      '$q',
+      '$httpBackend'
+    ];
+
+    addToWindow('tentacle', {});
+    addToWindow('async', async);
+    window.tentacle.mock = mock;
+    window.tentacle.inject = inject;
+    window.tentacle.injectAll = injectAll;
+    window.tentacle.controller = controller;
+  });
+
+  afterEach(function () {
+    deleteDirtyProperties();
+  });
+
+  function mock(module, name, override, defaultMock) {
+    var mocksObject = createMocksObject(dependencies, override, defaultMock);
+    _.forEach(mocksObject, provideValue);
+    return mocksObject;
+  }
+
+  function inject() {
+    _.forEach(globalInjects, injectDependency);
+  }
+
+  function injectAll(module, name) {
+    window.tentacle.inject();
+    _.forEach(getDependencies(module, name), injectDependency);
+    injectDependency(name);
+  }
+
+  function controller (module, name, override, defaultMock) {
+    window.tentacle.inject();
+    injectDependency('$controller');
+    var mocksObject = createMocksObject(module, name, override, defaultMock);
+    addToWindow('$scope', $rootScope.$new());
+    _.extend(mocksObject, {
+      $scope: window.$scope
+    });
+    window.tentacle.controller.run = function () {
+      window.$controller(name, mocksObject);
+    };
+    return mocksObject;
+  }
+
+  function async(x) {
+    if (!window.$q) {
+      injectDependency('$q');
+    }
+    return window.$q.when(x);
+  }
+
+  function addToWindow(propertyName, value) {
+    window[propertyName] = value;
+    dirtyProperties.push(propertyName);
+  }
+
+  function deleteDirtyProperties() {
+    _.forEach(dirtyProperties, function (prop) {
+      delete window[prop];
+    });
+  }
+
+  function injectDependency(dependency) {
+    angular.mock.inject([dependency, function (_dependency_) {
+      addToWindow(dependency, _dependency_);
+    }]);
+  }
+
+  function createMocksObject(module, name, override, defaultMock) {
     angular.mock.module(module);
     override = override || {};
     defaultVal = defaultVal || {};
 
     var dependencies = getDependencies(module, name);
     dependencies = _.difference(dependencies, mockExceptions);
-
-    var mocksObject = createMocksObject(dependencies, override, defaultMock);
-    _.forEach(mocksObject, provideValue);
-    return mocksObject;
-  };
-
-  window.tentacle.inject = function () {
-    _.forEach(globalInjects, injectDependency);
-  };
-
-  window.tentacle.injectAll = function (module, name) {
-    window.tentacle.inject();
-    _.forEach(getDependencies(module, name), injectDependency);
-    injectDependency(name);
-  };
-
-  function injectDependency(dependency) {
-    angular.mock.inject([dependency, function (_dependency_) {
-      window[dependency] = _dependency_;
-    }]);
+    return _.chain(dependencies)
+      .reduce(function (mockObject, dependency) {
+        mocksObject[dependency] = defaultMock;
+        return mocksObject;
+      }, {})
+      .assign(override)
+      .value();
   }
 
   function getDependencies(module, name) {
@@ -53,20 +112,9 @@
       .last().initial().value();
   }
 
-  function createMocksObject(dependencies, override, defaultMock) {
-    return _.chain(dependencies)
-      .reduce(function (mockObject, dependency) {
-        mocksObject[dependency] = defaultMock;
-        return mocksObject;
-      }, {})
-      .assign(override)
-      .value();
-  }
-
   function provideValue(mock, dependency) {
     angular.mock.module(function ($provide) {
       $provide.value(dependency, mock);
     });
   }
-
 })(window, angular);
